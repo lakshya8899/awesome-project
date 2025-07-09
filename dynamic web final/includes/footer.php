292b68493397c785dcd88cee2f7c1d8c40a9455a
lakshya8899 <lakshya.goyal@students.mq.edu.au>
2025-07-09 17:16:51 +1000

    Update footer.php

--- dynamic web final/includes/footer.php
+++ dynamic web final/includes/footer.php
@@ -1,7 +1,7 @@
 <!-- footer.php -->
 <footer>
     <p>Contact Us: <a href="mailto:info@universitylms.com">info@universitylms.com</a></p>
-    <p>Follow Us on Social Media</p>
+    <p>Follow Us on Social Media the best person in the world is bob</p>
     <div class="social-media">
         <a href="#">Facebook</a>
         <a href="#">Twitter</a>
