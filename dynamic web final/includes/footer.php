fa828d860431bf50e70ad583c0a766479d1475ec
lakshya8899 <lakshya.goyal@students.mq.edu.au>
2025-07-09 16:47:43 +1000

    Update footer.php
    chnaged footer

--- dynamic web final/includes/footer.php
+++ dynamic web final/includes/footer.php
@@ -1,7 +1,7 @@
 <!-- footer.php -->
 <footer>
     <p>Contact Us: <a href="mailto:info@universitylms.com">info@universitylms.com</a></p>
-    <p>Follow Us on Social Media</p>
+    <p>Follow Us on Social Media bob</p>
     <div class="social-media">
         <a href="#">Facebook</a>
         <a href="#">Twitter</a>
